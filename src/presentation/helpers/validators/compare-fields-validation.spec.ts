bd0af791ecb793998ec7ddb5b427d44d6e1cbd54
Geraldo Franca <geraldo.franca@mentorti.com.br>
2020-06-09 21:13:23 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,27 @@
+
+import { InvalidParamError } from '../../errors'
+import { CompareFieldValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldValidation => {
+  return new CompareFieldValidation('field', 'fieldToCompare')
+}
+
+describe('RequiredFieldValidation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_name',
+      fieldToCompare: 'wrong_value'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should rnot return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_name',
+      fieldToCompare: 'any_name'
+    })
+    expect(error).toBeFalsy()
+  })
+})
