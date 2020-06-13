c37df2722cf09ff77f21a4d4dfe55ed6cf4b65c9
Geraldo Franca <geraldo.franca@mentorti.com.br>
2020-06-13 08:12:35 -0300

    feat: ensure JwtAdapter returns a token when sign succeeds

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapater } from './jwt-adapter'
+
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
+describe('Jwt Adapter', () => {
+  test('Should call sign with correct values', async () => {
+    const sut = new JwtAdapater('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapater('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
+  })
+})
