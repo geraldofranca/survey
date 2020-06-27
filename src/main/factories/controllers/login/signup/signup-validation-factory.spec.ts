d82193fc1a92e41995766257724b6bf3e647269e
Geraldo Franca <geraldo.franca@mentorti.com.br>
2020-06-27 09:55:55 -0300

    refactor: organize main layer folders

--- src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -1,12 +1,12 @@
 import { makeSignUpValidation } from './signup-validation-factory'
-import { ValidationComposite } from '../../../../validation/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../../validation/validators/required-field-validation'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { CompareFieldValidation } from '../../../../validation/validators/compare-fields-validation'
-import { EmailValidation } from '../../../../validation/validators/email-validation'
-import { EmailValidator } from '../../../../validation/protocols/email-validator'
+import { ValidationComposite } from '../../../../../validation/validators/validation-composite'
+import { RequiredFieldValidation } from '../../../../../validation/validators/required-field-validation'
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { CompareFieldValidation } from '../../../../../validation/validators/compare-fields-validation'
+import { EmailValidation } from '../../../../../validation/validators/email-validation'
+import { EmailValidator } from '../../../../../validation/protocols/email-validator'
 
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
